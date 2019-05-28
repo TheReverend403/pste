283c640ad6f791f9873b653a4a81e5a705c05ba5
Lee Watson <rev@revthefox.co.uk>
2019-05-28 20:28:31 +0100

    Fix incorrect call to get().

--- app/models/user.py
+++ app/models/user.py
@@ -30,7 +30,7 @@ from app import db
 @login.header_loader
 def load_user_from_header(header):
     api_key = header.replace('Bearer ', '')
-    return User.query.filter_by(api_key=api_key).get()
+    return User.query.filter_by(api_key=api_key).first()
 
 
 @login.user_loader
