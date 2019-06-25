2452991a620db0450365a3f888e8e82e8afe2210
Lee Watson <rev@revthefox.co.uk>
2019-06-25 21:00:10 +0100

    Add .env support

---
+++ app/settings.py
@@ -0,0 +1,34 @@
+#  This file is part of pste.
+#
+#  pste is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  pste is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with pste.  If not, see <https://www.gnu.org/licenses/>.
+
+from environs import Env
+
+env = Env()
+env.read_env()  # read .env file, if it exists
+
+SECRET_KEY = env.str('SECRET_KEY')
+SQLALCHEMY_DATABASE_URI = env.str('DATABASE_URI')
+
+with env.prefixed('MAIL_'):
+    MAIL_SERVER = env.str('SERVER')
+    MAIL_PORT = env.int('PORT')
+    MAIL_USE_TLS = env.bool('USE_TLS')
+    MAIL_USERNAME = env.str('USERNAME')
+    MAIL_PASSWORD = env.str('PASSWORD')
+    MAIL_DEFAULT_SENDER = env.str('FROM')
+
+# Sentry
+# https://sentry.io
+SENTRY_DSN = ''
