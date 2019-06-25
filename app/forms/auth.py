9ecc35bc85b68bac2ecfc02b61613314bc52bbd0
Lee Watson <rev@revthefox.co.uk>
2019-06-25 20:45:32 +0100

    Remove recaptcha

--- app/forms/auth.py
+++ app/forms/auth.py
@@ -13,7 +13,7 @@
 #  You should have received a copy of the GNU General Public License
 #  along with pste.  If not, see <https://www.gnu.org/licenses/>.
 
-from flask_wtf import FlaskForm, RecaptchaField
+from flask_wtf import FlaskForm
 from wtforms import PasswordField, validators, BooleanField, SubmitField
 from wtforms.fields.html5 import EmailField
 
