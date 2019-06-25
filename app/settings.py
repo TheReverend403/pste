21801df738d9a693e59a47bfc5c55ccdc429fc75
Lee Watson <rev@revthefox.co.uk>
2019-06-25 21:09:24 +0100

    Don't track SQLAlchemy modifications

--- app/settings.py
+++ app/settings.py
@@ -20,6 +20,7 @@ env.read_env()
 
 SECRET_KEY = env.str('SECRET_KEY')
 SQLALCHEMY_DATABASE_URI = env.str('DATABASE_URI')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 with env.prefixed('MAIL_'):
     MAIL_SERVER = env.str('SERVER')
