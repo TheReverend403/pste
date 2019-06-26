aa8b92d12b27a2b23d9476f036ad260dcaa60e3d
Lee Watson <rev@revthefox.co.uk>
2019-06-26 21:34:55 +0100

    If a file is already uploaded by a user, just update the name of the existing file.

--- app/views/api.py
+++ app/views/api.py
@@ -44,6 +44,12 @@ def upload():
     file_hash = hashlib.sha256(fd.read()).hexdigest()
     fd.seek(0)
 
+    existing_file = File.query.filter_by(user=current_user, file_hash=file_hash)
+    if existing_file:
+        existing_file.name = fd.filename
+        db.session.commit()
+        return jsonify({'url': url_for('web.file', slug=existing_file.slug, _external=True)})
+
     file = File(user=current_user)
     file.name = fd.filename
     file.size = file_size
